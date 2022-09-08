bb276a882ef598f2440ff85d0321b35ab40c557e
Harold Adjahi <harldadjahi@gmail.com>
2022-09-08 16:15:48 -0700

    hello world task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-/** This program  writes out  a simple string
-  */
+/* This program  writes out  a simple string */
 int main(void)
 {
  puts ("\"Programming is like building a multilingual puzzle");
