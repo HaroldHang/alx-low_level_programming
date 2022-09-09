0e0529f8381e71e9581db062466fffe2f92df966
Harold Adjahi <harldadjahi@gmail.com>
2022-09-09 16:35:45 -0700

    variables if else while task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Print alphabet lowercase except q and e
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int count  = 0x61;
+char letter;
+int i;
+char line;
+/* your code goes there */
+for (i = 0 ; i < 26; i++)
+{
+letter = count;
+if ( i == 4 || i == 16)
+{
+continue;
+}  
+putchar(letter);
+count++;
+}
+count = 0x0a;
+line = count;
+putchar(line);
+return (0);
+}
