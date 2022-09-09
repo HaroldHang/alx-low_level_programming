bad1f8f4e3a0a4bf98b5587c99d62fa50b932fdf
Harold Adjahi <harldadjahi@gmail.com>
2022-09-09 16:30:58 -0700

    variables if else while task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Print alphabet in lower and uppercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int count  = 0x61;
+char letter;
+int i;
+char line;
+/* your code goes there */
+for (i = 0 ; i < 26; i++)
+{
+letter = count;
+putchar(letter);
+count++;
+}
+count = 0x41;
+for (i = 0 ; i < 26; i++)
+{
+letter = count;
+putchar(letter);
+count++;
+}
+count = 0x0a;
+line = count;
+putchar(line);
+return (0);
+}
