a60edf46d58108b6ecd206d15c0f2a60e7c628f2
Harold Adjahi <harldadjahi@gmail.com>
2022-09-13 14:26:47 -0700

    functions nested lopps task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Print _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  
+/* your code goes there */
+print("_putchar");
+return (0);
+
+}
