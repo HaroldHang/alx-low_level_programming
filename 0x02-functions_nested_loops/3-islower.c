8540cfc6547f54a6410c4dba1a6d2bfdac779b41
HaroldHang <haroldadjahi@gmail.com>
2022-09-14 00:35:57 +0100

    functions nested loops task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (islower(c) == 0) {
+return 0;
+} else if (islower(c) > 0) {
+return 1;
+}
+}
