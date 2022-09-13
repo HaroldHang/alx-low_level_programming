82ca3c7aa91bd1c31d45920fbf00e8d5d9c748d6
HaroldHang <haroldadjahi@gmail.com>
2022-09-13 23:54:03 +0100

    functions nested loops task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,9 +10,10 @@
  * Print _putchar
  * Return: Always 0 (Success)
  */
-void print_alphabet();
+void print_alphabet(void);
 
-int main(void) {
+int main(void) 
+{
 /* your code goes there */
 print_alphabet();
 return (0);
