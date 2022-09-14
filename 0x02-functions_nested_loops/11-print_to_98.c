6f5f2814aefc61320dee66952a72db696eafe516
HaroldHang <haroldadjahi@gmail.com>
2022-09-15 00:55:54 +0100

    functions nested loops task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,90 +9,18 @@
  */
 void print_to_98(int n)
 {
-int i; 
-int k;
-if (n <= 98)
+while (n != 98)
 {
-for (i = n; i <= 98; i++)
+printf("%i, ", n);
+if (n > 98)
 {
-if (i > n)
-{
-_putchar(32);
-}
-if (i < 0)
-{
-_putchar(45);
-k = -(i);
+n--;
 }
 else
 {
-k = i;
+n--;
 }
-print_num(k, 1);
 }
-_putchar(10);
-}
-else
-{
-for (i = n; i >= 98; i--)
-{
-if (i < n)
-{
-_putchar(32);
-}
-print_num(i, 0);
-}
-_putchar(10);
-}
+printf("98\n");
 }
 
-/**
- * print_num - Print num
- *@num: Where to print ftom
- *@after: Where to print ftom
- * Return: Always 0 (Success)
- */
-
-void print_num(int num, int after)
-{
-int l;
-int t;
-if (num >= 100)
-{
-l = num / 10;
-t = l % 10;
-l = l / 10;
-_putchar(l + 48);
-_putchar(t + 48);
-t = num % 10;
-_putchar(t + 48);
-}
-else if (num >= 10)
-{
-l = num / 10;
-_putchar(l + 48);
-t = num % 10;
-_putchar(t + 48);
-}
-else
-{
-_putchar(num + 48);
-}
-if (after == 1)
-{
-if (num < 98)
-{
-_putchar(44);
-}
-}
-else
-{
-if (after == 0)
-{
-if (num > 98)
-{
-_putchar(44);
-}    
-}
-}
-}
