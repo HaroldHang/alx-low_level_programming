4c8fd39c37b1a7dfb398b8629d620baee318164b
HaroldHang <haroldadjahi@gmail.com>
2022-09-14 01:01:19 +0100

    functions nested loops task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _abs - absolute value
+ *@n: the character to be checked
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+if (n < 0) {
+n = n * (-1);
+return (n);
+}
+else
+{
+return (n);
+}
+return (0);
+}
