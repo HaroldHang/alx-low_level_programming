a028763c03e7ea6c81c9f8fe3674394028d565d4
HaroldHang <haroldadjahi@gmail.com>
2022-09-14 02:02:00 +0100

    functions nested loops task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * add - Return the sum of two numbers
+ *@a: first parameter
+ *@b: second parameter
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+return 0;
+}
