c6d00d4e68cae9ca4b1c36f068b096a0dc5116df
HaroldHang <haroldadjahi@gmail.com>
2022-09-14 01:52:15 +0100

    functions nested loops task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * times_table - Print the 0 times tables
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int i;
+int n;
+int j;
+int k;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+n = i * j;
+if (n >= 10)
+{
+k = n / 10;
+n = n % 10;
+_putchar(k + 48);
+_putchar(n + 48);
+}
+else
+{
+_putchar(n + 48);
+}
+if (j < 9)
+{
+_putchar(44);
+}
+}
+_putchar(10);
+}
+
+}
