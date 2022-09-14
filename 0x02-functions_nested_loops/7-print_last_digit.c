88346bd8e1aaedb0451befc8f92420cf578548fb
HaroldHang <haroldadjahi@gmail.com>
2022-09-14 01:07:42 +0100

    functions nested loops task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit - Print the last digit of the integer
+ *@n: the integer to be checked
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+if (n < 0) 
+{
+n = - (n);
+}
+return (n);
+return (0);
+}
