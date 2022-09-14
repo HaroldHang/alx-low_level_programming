564d3055c30858324ea02b44e15eed6bb8855ee2
HaroldHang <haroldadjahi@gmail.com>
2022-09-14 02:46:09 +0100

    functions nested loops task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,6 @@ void print_to_98(int n)
 {
 int i;
 int k;
-int l;
-int t;
 if (n <= 98)
 {
 for (i = n; i <= 98; i++)
@@ -21,7 +19,8 @@ if (i > n)
 {
 _putchar(32);
 }
-if (i < 0) {
+if (i < 0)
+{
 _putchar(45);
 k = -(i);
 }
@@ -29,27 +28,7 @@ else
 {
 k = i;
 }
-if (k >= 100)
-{
-l = k / 10;
-t = l % 10;
-l = l / 10;
-_putchar(l + 48);
-_putchar(t + 48);
-k = k % 10;
-_putchar(k + 48);
-}
-else if (k >= 10)
-{
-l = k / 10;
-_putchar(l + 48);
-k = k % 10;
-_putchar(k + 48);
-}
-else
-{
-_putchar(k + 48);
-}
+print_num(k);
 if (i < 98)
 {
 _putchar(44);
@@ -65,27 +44,45 @@ if(i < n)
 {
 _putchar(32);
 }
-if (i >= 100)
-{
-l = i / 10;
-k = l % 10;
-l = l / 10;
-_putchar(l + 48);
-_putchar(k + 48);
-k = i % 10;
-_putchar(k + 48);
-} else if (i >= 10)
-{
-l = i / 10;
-_putchar(l + 48);
-k = i % 10;
-_putchar(k + 48);
-}
+print_num(i);
 if (i > 98)
 {
 _putchar(44);
 }
 }
 _putchar(10);
+}
+}
+
+/**
+ * print_num - Print num
+ *@num: Where to print ftom
+ * Return: Always 0 (Success)
+ */
+
+void print_num(int num)
+{
+int l;
+int t;
+if (num >= 100)
+{
+l = num / 10;
+t = l % 10;
+l = l / 10;
+_putchar(l + 48);
+_putchar(t + 48);
+num = num % 10;
+_putchar(num + 48);
+}
+else if (num >= 10)
+{
+l = num / 10;
+_putchar(l + 48);
+num = num % 10;
+_putchar(k + 48);
+}
+else
+{
+_putchar(num + 48);
 }
 }
