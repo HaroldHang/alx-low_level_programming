a6fede8ccc6f4e51005f93d17fdfbf88261fe8d2
HaroldHang <haroldadjahi@gmail.com>
2022-09-15 02:06:17 +0100

    functions nested loops task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,10 +10,7 @@
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-int k;
-int t;
+int i, j, k, t, s;
 if (n > 0 && n < 15)
 {
 for (i = 0; i <= n; i++)
@@ -27,13 +24,27 @@ _putchar(32);
 if (k < 10)
 {
 _putchar(32);
+_putchar(32);
+} else if (k >= 10 && < 100)
+{
+_putchar(32);
 }
+}
+if (k >= 100)
+{
+t = k % 10;
+s = t / 10;
+_putchar(s + 48);
+s = t % 10;
+_putchar(s + 48);
+t = k % 100;
+_putchar(k + 48);
 }
 if (k >= 10)
 {
-k = k / 10;
-_putchar(k + 48);
-t = t % 10;
+t = k / 10;
+_putchar(t + 48);
+k = k % 10;
 _putchar(t + 48);
 }
 else
