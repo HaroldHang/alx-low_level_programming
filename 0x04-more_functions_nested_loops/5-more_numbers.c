3e90bc2b63d9284a0aff12522778ec917b5c3fd8
HaroldHang <haroldadjahi@gmail.com>
2022-09-16 00:45:45 +0100

    more function task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,16 +13,16 @@ for (j = 0; j < 10; j++)
 {
 for (i = 0; i <= 14; i++)
 {
-k = i / 10;
-m = i % 10;
 if (i > 9)
 {
-_putchar(k + 48);
-_putchar(m + 48);
+k = i / 10;
+m = i % 10;
+_putchar(k + '0');
+_putchar(m + '0');
 }
 else
 {
-_putchar(i + 48);
+_putchar(i + '0');
 }
 }
 _putchar('\n');
