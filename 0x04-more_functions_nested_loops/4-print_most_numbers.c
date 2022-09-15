a79cbbb945884a38eefa3296967da37b62e2adb8
HaroldHang <haroldadjahi@gmail.com>
2022-09-16 00:26:26 +0100

    more function task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_most_numbers - print the numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i == '2' || i == '4')
+{
+continue;
+}
+_putchar(i);
+}
+_putchar('\n');
+}
