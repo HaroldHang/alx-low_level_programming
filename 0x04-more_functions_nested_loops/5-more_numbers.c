2e0e94b9e1cb3591a3ebcecc88d5a25daf044a24
HaroldHang <haroldadjahi@gmail.com>
2022-09-16 00:50:15 +0100

    more function task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,4 @@
 #include "main.h"
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
  * more_numbers - print the numbers from 0 to 14 ten times
  * Return: Always 0 (Success)
