9ebdabe98686d3210eff74a97269d367208a20ec
HaroldHang <haroldadjahi@gmail.com>
2022-09-16 00:14:00 +0100

    more function task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * mul - Return the multiplication of two numbers
+ *@a: first parameter
+ *@b: second parameter
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+return (0);
+}
