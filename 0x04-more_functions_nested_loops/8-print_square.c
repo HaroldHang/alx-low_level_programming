36eff72c8947ca6a40151af1c276ec9731793653
HaroldHang <haroldadjahi@gmail.com>
2022-09-16 01:37:55 +0100

    more function task8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,23 +13,9 @@ for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-if (i == 0 || i == (n - 1))
-{
-_putchar('#');  
-}
-else
-{
-if (j > 0 && j < (n - 1))
-{
-_putchar(' ');
-}
-else
-{
 _putchar('#');
 }
-}
 _putchar('\n');
-}
 }
 }
 else
