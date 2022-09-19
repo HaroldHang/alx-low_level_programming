bb215d058f0779bc0e79330124a0c255082b73e1
HaroldHang <haroldadjahi@gmail.com>
2022-09-19 23:44:46 +0100

    pointers task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 /**
- *reset_to_98 - Reset a pointer 
- *@n: pointer to the variable
- * Return: Always 0.
+ * reset_to_98 - Change the external variable
+ *@n: the pointer to the variable
+ * Return: Always 0 (Success)
  */
 
 void reset_to_98(int *n)
