2d72c61ca47b501eb4926ef19bd769db43fb3744
HaroldHang <haroldadjahi@gmail.com>
2022-09-19 23:48:47 +0100

    pointers task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - Swap two variables
+ *@a: the pointer to the first variable
+ *@b: the pointer to the second variable
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp; 
+}
