61d69841336792b5227abc4120ee6624ccdfa031
HaroldHang <haroldadjahi@gmail.com>
2022-09-20 02:02:17 +0100

    pointers task6

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+/**
+ * print_array - Print half of string
+ *@a: the array to pint
+ *@n: the n elements
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i == (n-1))
+{
+printf("%d", a[i]);
+}
+else
+{
+printf("%d, ", a[i]);
+}
+}
+}
