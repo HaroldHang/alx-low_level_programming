1fb064c9d232027cb34c4db99d0b4e576bb3417e
HaroldHang <haroldadjahi@gmail.com>
2022-09-20 02:11:12 +0100

    pointers task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - string copying
+ *@dest: the destination to print
+ *@src: the string source
+ * Return: Always 0 (Success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
