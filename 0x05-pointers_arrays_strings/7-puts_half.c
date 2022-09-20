0af794b4a4cf4de93527a38b5fa6f0d9fb3f22dd
HaroldHang <haroldadjahi@gmail.com>
2022-09-20 01:53:16 +0100

    pointers task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - Print half of string
+ *@str: the string to be half
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+int count = strlen(s) - 1;
+int i;
+if (i % 2 == 0)
+i = count / 2;
+else
+i = (count - 1) / 2;
+for (; i <= count; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
