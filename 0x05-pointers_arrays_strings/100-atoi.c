07d355c8273f9d475c3987a6f829dcb71213802d
HaroldHang <haroldadjahi@gmail.com>
2022-09-21 01:12:33 +0100

    pointer array string task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _atoi - string to number
+ *@s: the string to convert in number
+ * Return: Always 0 (Success)
+ */
+
+int _atoi(char *s)
+{
+int sign = 1;
+int num;
+num = atoi(*s);
+if (num == 0) {
+while (*s != '\0')
+{
+if (*s == '-')
+{
+sign = -1;
+}
+else if (*s >= '0' && *s <= '9')
+{
+num = (num * 10) + *s - '0';
+}
+s++;
+}
+return (num);
+}
+else
+{
+return (num);
+}
+}
