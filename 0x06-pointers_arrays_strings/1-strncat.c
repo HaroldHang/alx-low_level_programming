7c40767e3b40c0841c24b8884bf3b5b803cafb8e
HaroldHang <haroldadjahi@gmail.com>
2022-09-22 21:07:26 +0100

    more pointers arrays string task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,10 +4,11 @@
  * _strncat - string contatenation
  *@dest: the destination to print
  *@src: the string source
+ *@n: n bytes to concatenate
  * Return: Always 0 (Success)
  */
 
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-return (strncat(dest, src));
+return (strncat(dest, src, n));
 }
