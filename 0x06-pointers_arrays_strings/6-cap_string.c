5173a1fbb90770aaf44c941d41b59e84dc26f0c8
HaroldHang <haroldadjahi@gmail.com>
2022-09-23 00:16:58 +0100

    more pointers arrays string task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * cap_string - string capitalization
+ *@s: string to capitalize
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *s)
+{
+char *temp = s;
+while (*s)
+{
+if (((*s == " ") || (*s == "\t") || (*s == "\n") || (*s == ',') || (*s == ';') || (*s == '.') (*s == '!') || (*s == '?') || (*s == '"') || (*s == '(') || (*s == ')') || (*s == '{') || (*s == '}')) && (*(s + 1) >= 'a' && *(s + 1) <= 'z'))
+{
+*(s + 1) = *(s + 1) - 32;
+}
+s++;
+}
+return (temp);
+}
+
