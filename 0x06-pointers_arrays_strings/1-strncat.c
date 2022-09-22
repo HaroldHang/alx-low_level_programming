09c616262740f307a5ca1301ac6626a0ee18fe10
HaroldHang <haroldadjahi@gmail.com>
2022-09-22 21:04:54 +0100

    more pointers arrays string task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncat - string contatenation
+ *@dest: the destination to print
+ *@src: the string source
+ * Return: Always 0 (Success)
+ */
+
+char *_strncat(char *dest, char *src)
+{
+return (strncat(dest, src));
+}
