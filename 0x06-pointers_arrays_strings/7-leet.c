fc0c0d3a06ec96a97bd4c68344650dcfc2715913
HaroldHang <haroldadjahi@gmail.com>
2022-09-23 01:46:38 +0100

    more pointers arrays string task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,12 +8,12 @@ char *leet(char *s)
 {
 char *temp = s;
 int i;
-char letters[5] = {'A' , 'E', 'O', 'T', 'L'};
-char numbers[5] = {'4' , '3', '0', '7', '1'};
+char letters[5] = { 'A' , 'E', 'O', 'T', 'L' };
+char numbers[5] = { '4' , '3', '0', '7', '1' };
 while (*s)
 {
 for (i = 0; i < 5; i++) {
-if ((*s == letters[i]) || (*s == (letters[i] + 32)))
+if (*s == letters[i] || *s == (letters[i] + 32))
 *s = numbers[i];
 }
 s++;
