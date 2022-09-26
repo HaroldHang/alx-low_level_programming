26ac2703a131ff91db550569c9061212d8e2ecaa
HaroldHang <haroldadjahi@gmail.com>
2022-09-26 22:35:30 +0100

    even even  more pointers task6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strstr Locate a substring
+ *@a: board to print
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; i < 8; j++)
+{
+putchar('%c', a[i][j]);
+}
+putchar('\n');
+}
+}
