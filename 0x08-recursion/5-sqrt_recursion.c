d2b529deb555959fbd36177b4f3523ec5a820ebd
HaroldHang <haroldadjahi@gmail.com>
2022-09-29 00:37:08 +0100

    recursion task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ *_sqrt_recursion - square root
+ *@n: the number
+ * Return: Always 0 (Success)
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return (_sqrt(n, 1));
+}
+
+int _sqrt(int num, int checker)
+{
+int numEqual = checker * checker;
+if (numEqual > num)
+return (-1);
+else if (numEqual < num)
+return _sqrt(num, checker + 1);
+else
+return checker;
+}
