50dd6829fe84d019d28111bad698a101ed02c757
HaroldHang <haroldadjahi@gmail.com>
2022-09-28 23:42:46 +0100

    recursion task3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *factorial - factorial of a number
+ *@n: the number
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+if (n == 1)
+return (1);
+else
+return (factorial (n-1));
+}
