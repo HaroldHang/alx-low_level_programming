9e1ac190e144fb5fba0885905ca4537df92cdf30
HaroldHang <haroldadjahi@gmail.com>
2022-09-28 23:26:09 +0100

    recursion task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_print_rev_recursion - print a string
+ *@s: the string to print
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(s);
+}
+}
