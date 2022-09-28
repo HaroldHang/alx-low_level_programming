744deb07899c7fdfb860865610f0eefd639c408a
HaroldHang <haroldadjahi@gmail.com>
2022-09-28 23:52:59 +0100

    recursion task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_pow_recursion - power of x by y
+ *@x: the first number
+ *@y: the second number
+ * Return: Always 0 (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else
+return pow(x, y);
+}
