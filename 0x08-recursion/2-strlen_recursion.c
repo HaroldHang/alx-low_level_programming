862830a59bcc1ddfbba96e98386ccd3b37c3cadb
HaroldHang <haroldadjahi@gmail.com>
2022-09-28 23:33:24 +0100

    recursion task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *_strlen_recursion - length of string
+ *@s: the string to find length
+ * Return: Always 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
