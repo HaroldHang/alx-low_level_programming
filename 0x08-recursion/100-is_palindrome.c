4a1529bb07345c1c4f625633294e3a96491fbf3c
HaroldHang <haroldadjahi@gmail.com>
2022-10-01 00:37:54 +0100

    static librairies

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+char rev(char *str);
+/**
+ *is_palindrome - check if a palindrome
+ *@s: the string
+ * Return: Always 0 (Success)
+ */
+int is_palindrome(char *s)
+{
+
+}
+
+/**
+ *rev - performs the reverse string
+ *@str: the string
+ *@start: the start number
+ * Return: Always 0 (Success)
+ */
+char rev(char *s)
+{
+char
+}
