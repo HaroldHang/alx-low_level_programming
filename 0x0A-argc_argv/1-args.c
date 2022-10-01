ab32ea530d2ec708c857b6892839794cec371034
HaroldHang <haroldadjahi@gmail.com>
2022-10-01 01:47:54 +0100

    argc argv task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the argument
+ *
+ * Return: Always 0
+ */
+int main (int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%s\n", argc);
+return (0);
+}
