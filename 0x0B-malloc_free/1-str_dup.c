3559fdbbe01ea8e495f52795fbd2c6bf9d600230
HaroldHang <haroldadjahi@gmail.com>
2022-10-04 23:23:50 +0100

    malloc task1

--- 0x0B-malloc_free/1-str_dup.c
+++ 0x0B-malloc_free/1-str_dup.c
@@ -11,6 +11,7 @@ char *_strdup(char *str)
 {
 int i;
 char *s;
+i = 0;
 s = malloc(sizeof(str));
 if (str == NULL || s == NULL)
 {
@@ -18,9 +19,9 @@ return (NULL);
 }
 else
 {
-while (i < size)
+while (i < sizeof(str))
 {
-s[i] = c;
+s[i] = str[i];
 i++;
 }
 return  (s);
