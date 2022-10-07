41da93988ee5c810df6e5aadc2a1dbf5fe541fcd
HaroldHang <haroldadjahi@gmail.com>
2022-10-07 08:46:26 +0100

    more malloc task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,19 +12,20 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-if (s1 == NULL || s2 == NULL)
-{
+if (s1 == NULL)
 s1 = "";
+
+if (s2 == NULL)
 s2 = "";
-}
-s = malloc(sizeof(char) * (sizeof(s1) + (n + 1)));
+
 if (n >= strlen(s2))
+n = strlen(s2);
+
+s = malloc(sizeof(char) * (sizeof(s1) + (n + 1)));
+if (s == NULL)
 {
-s = strcat(s1, s2);
+return (NULL);
 }
-else
-{
-s = strcat(s1, s2, n);
-}
+s = strncat(s1, s2, n);
 return (s);
 }
