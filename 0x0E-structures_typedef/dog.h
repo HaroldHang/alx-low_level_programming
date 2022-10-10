a0662e0429616dd6299c1aaa8e0ba0b1f31e8a0b
HaroldHang <haroldadjahi@gmail.com>
2022-10-11 00:02:15 +0100

    struct task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+#endif
+
+/**
+ * struct dog - Dog properties
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: The dog with its characterisitcs
+ */
+
+struct dog {
+char *name,
+float age,
+char *owner
+}
