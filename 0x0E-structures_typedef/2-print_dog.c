6c1b1a23851fda1428d4505f60295564f9d309b6
HaroldHang <haroldadjahi@gmail.com>
2022-10-11 02:53:03 +0100

    struct task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 /**
  * print_dog - print a dog
  * @d: the struct of dog
@@ -11,10 +11,10 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return;
 if (d->name == NULL)
-d->name="(nil)";
+d->name = "(nil)";
 
 if (d->owner == NULL)
 d->owner = "(nil)";
 
-printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner)
+printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
