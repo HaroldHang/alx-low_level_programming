a4970f03171332185cd4ed3e2c4e80a4f6e1b7ee
HaroldHang <haroldadjahi@gmail.com>
2022-10-11 02:27:53 +0100

    struct task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ printf("Name: %s\n", d->name);
 if (d->age == NULL)
 printf("Age: (nil)\n");
 else
-printf("Age: %2f\n", d->age);
+printf("Age: %0.6f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
