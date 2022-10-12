4246e09ba6ae92e08422d36d555ef1d42dc0f0d7
HaroldHang <haroldadjahi@gmail.com>
2022-10-12 23:58:01 +0100

    function pointers task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,17 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 /**
  * print_name - print the name
  * @name: the name to print
  * @f: the funtion to print with
- * @owner: the owner of dog
  *
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+if (name == NULL || f == NULL)
+{
+return;
+}
+f(name);
 }
