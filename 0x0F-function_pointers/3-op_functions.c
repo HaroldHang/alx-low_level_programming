efa8a143e353eb06005233c8b90da99bdeebcf8d
HaroldHang <haroldadjahi@gmail.com>
2022-10-13 01:10:26 +0100

    function pointers task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -25,17 +25,6 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_sub - a function that returns the subtract of a and b.
- * @a: an input integer
- * @b: an input integer
- * Return: The result to substract a - b.
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
  * op_mul - a function that returns the multiply of a and b.
  * @a: an input integer
  * @b: an input integer
