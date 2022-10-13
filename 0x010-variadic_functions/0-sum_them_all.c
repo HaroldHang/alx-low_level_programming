8f27b54f57458f5aae569da12e09dcd29148a171
HaroldHang <haroldadjahi@gmail.com>
2022-10-13 23:59:23 +0100

    variadic task0

---
+++ 0x010-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_them_all - sum all optional arguments
+ * @n: the number of arguments
+ * Return: some of numnbers
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+{
+return (0);
+}
+int sum = 0;
+va_list ptr;
+va_start(ptr, n);
+for (unsigned int i = 0; i < n; i++)
+{
+sum += va_arg(ptr, int);
+}
+va_end(ptr);
+return (sum);
+}
+
