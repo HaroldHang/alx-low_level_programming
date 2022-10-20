761a28755b2e8358a32d774e5d3aa8eedc08a848
HaroldHang <haroldadjahi@gmail.com>
2022-10-20 23:21:30 +0100

    linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * add_node - Add a note to the list
+ * @head: The head of a linked list.
+ * @str: The value of the node.
+ * Return: the pointer to the new list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_head;
+    new_head = malloc(sizeof(list_t));
+    if (new_head == NULL)
+    {
+        return (NULL);
+    }
+    new_head->str = strdup(str);
+    new_head->len = strlen(str);
+    new_head->next = *head;
+    *head = new_head;
+    return (new_head);
+}
