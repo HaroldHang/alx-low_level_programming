0d5a8402c9280550d3b713868f83d240ea14756e
HaroldHang <haroldadjahi@gmail.com>
2022-10-20 22:57:46 +0100

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-    int n = 0;
+    unsigned int n = 0;
 	while(h != NULL)
     {
         printf("[%d] %s\n", h->len, h->str);
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,7 +8,7 @@
  */
 size_t list_len(const list_t *h)
 {
-    int n = 0;
+    unsigned int n = 0;
 	while(h != NULL)
     {
         h = h->next;
