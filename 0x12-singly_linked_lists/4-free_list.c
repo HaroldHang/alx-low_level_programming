63d7ffa27cd85c4b573f691b73fcdcfbac78737b
HaroldHang <haroldadjahi@gmail.com>
2022-10-21 00:28:16 +0100

    linked list free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * add_node_end - Add a note to the list
+ * @head: The head of a linked list.
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+    if (head == NULL)
+    {
+        free(head);
+        return;
+    }
+    while (head->next != NULL)
+    {
+        free_list(head);
+    }
+    free(head);
+}
