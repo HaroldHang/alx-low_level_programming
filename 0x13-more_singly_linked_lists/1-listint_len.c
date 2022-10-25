4353a4a288e3d33524c28411dadf6fa8f49fcb88
HaroldHang <haroldadjahi@gmail.com>
2022-10-25 23:38:54 +0100

    more linked list len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,7 +6,7 @@
  * @h: The head of a linked list.
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 {
     size_t count = 0;
 	while(h)
