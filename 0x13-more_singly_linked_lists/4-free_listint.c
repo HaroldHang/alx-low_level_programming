a4a9b29996c31961bc2f237eee5e7f7686368ab7
HaroldHang <haroldadjahi@gmail.com>
2022-10-26 00:03:41 +0100

    more linked list add node end

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * add_node_end - Add a note to the list
+ * @head: The head of a linked list.
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+    listint_t *temp;
+    if (head == NULL)
+    {
+        return;
+    }
+    while (head->next != NULL)
+    {
+        temp = head->next;
+        free(head->n);
+        free(head);
+        free_list(temp);
+    }
+    free(head);
+}
