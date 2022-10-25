5a8cd89fadb11605860346fb062a4b876a4d4e59
HaroldHang <haroldadjahi@gmail.com>
2022-10-26 00:22:11 +0100

    more linked list free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * free_listint2 - Add a note to the list
+ * @head: The head of a linked list.
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+    listint_t *temp;
+    if (*head == NULL)
+    {
+        return;
+    }
+    while ((*head)->next != NULL)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
+    free(*head);
+}
+
