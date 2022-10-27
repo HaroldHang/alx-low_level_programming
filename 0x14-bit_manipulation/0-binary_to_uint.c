81e559655033c64d99a394e6fca070aa6c663362
HaroldHang <haroldadjahi@gmail.com>
2022-10-27 23:41:06 +0100

    bit manip 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 
 /**
  * binary_to_uint - A function that sum the list
@@ -13,27 +13,23 @@ unsigned int binary_to_uint(const char *b)
     {
         return (0);
     }
-	unsigned int conv = 0;
-
-	int len;
+    unsigned int conv = 0;
+    int len;
     len = strlen(b);
     unsigned int bin = 1;
-  //len--;
-
-  //printf("%c", b[i]);
-	while (len)
-	{
-		if (b[len - 1] != '1' && b[len - 1] != '0')
+    while (len)
     {
-        return (0);
+      if (b[len - 1] != '1' && b[len - 1] != '0')
+      {
+          return (0);
+      }
+      if (b[len - 1] == '1')
+      {
+          conv += 1 * bin;
+      }
+      bin *= 2;
+      len--;
     }
-    if (b[len - 1] == '1')
-    {
-      conv += 1 * bin;
-    }
-    bin *= 2;
-        len--;
-	}
-	return (conv);
+    return (conv);
 }
 
