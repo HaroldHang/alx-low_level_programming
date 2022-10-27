bbcab3394661a31467c9764f84c0303c8e13727d
HaroldHang <haroldadjahi@gmail.com>
2022-10-28 00:54:59 +0100

    bit manip 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * get_bit - Print binary number
+ * @n: The number
+ * @index: The index of bit
+ * Return: the bit at position
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{   
+    unsigned int flag = 0, max = 32768; 
+    unsigned int pos = 15;
+	
+	while (max)
+	{
+        
+        if (pos == index)
+        {
+            if ((n & max) != 0)
+            {
+                return (1);
+            }
+            else
+            {
+                return (0);
+            }
+        }
+        else
+        {
+            pos--;
+        }
+        
+		
+        max >>= 1;
+	}
+}
+
