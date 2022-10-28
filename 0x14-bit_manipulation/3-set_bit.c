bc6b23deccaef802c3a80494595bf923b9072920
HaroldHang <haroldadjahi@gmail.com>
2022-10-28 01:30:28 +0100

    bit manip 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - Set the bit
+ * @n: The number
+ * @index: The index of bit
+ * Return: the bit at position
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{   
+    unsigned int max = 0x01;
+    max <<= index;
+    if (max == 0)
+    return (-1);
+    n = n | max;
+    return (n);
+}
+
