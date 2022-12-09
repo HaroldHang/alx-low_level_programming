2ebaa649754e8c92f5e741c0d4978f00ac29a8a0
HaroldHang <haroldadjahi@gmail.com>
2022-12-09 03:06:19 +0100

    doubled linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,10 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    unsigned int count = 0;
+    size_t count = 0;
+    if (!h) {
+        return (count);
+    }
 	while(h)
     {
         printf("%d\n", h->n);
